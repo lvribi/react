20cc3d8bb11534c04f8ffcffed6b54b0ca51f1e3
Test Lvribi <alaalaaribi@gmail.com>
2022-03-01 13:53:21 +0100

    commit section 3

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,21 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import MyComponent from "../MyComponent";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const num = 1;
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpenseItem expense={props.expenseData[0]}></ExpenseItem>
+        <ExpenseItem expense={props.expenseData[1]}></ExpenseItem>
+        <ExpenseItem expense={props.expenseData[2]}></ExpenseItem>
+        <ExpenseItem expense={props.expenseData[3]}></ExpenseItem>
+      </Card>
+      <MyComponent num={num}></MyComponent>
+    </div>
+  );
+};
+
+export default Expenses;
